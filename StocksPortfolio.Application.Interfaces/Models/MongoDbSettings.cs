4ff949cea109156e66c7986148e391483f9f3f43
Oliwier Kania <o2k.oliwierkacperkania@gmail.com>
2024-03-11 09:05:49 +0100

    Added database configuration and api connection configuration to appsettings.

---
+++ StocksPortfolio.Application.Interfaces/Models/MongoDbSettings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksPortfolio.Application.Interfaces.Models
+{
+    public class MongoDbSettings
+    {
+        public string PortfolioName { get; set; }
+        public string[] PortfolioConnectionString { get; set; }
+        public string PortfolioCollectionName { get; set; }
+
+        public string CurrencyName { get; set; }
+        public string[] CurrencyConnectionString { get; set; }
+        public string CurrencyCollectionName { get; set; }
+
+        public string[] PortfolioSaveString { get; set; }
+        public string[] CurrencySaveString { get; set; }
+    }
+}
