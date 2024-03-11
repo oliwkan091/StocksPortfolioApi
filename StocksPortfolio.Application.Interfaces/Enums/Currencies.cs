3042c7e10db6172f8d89cafc92cf051c29a13def
Oliwier Kania <o2k.oliwierkacperkania@gmail.com>
2024-03-11 09:03:59 +0100

    Added enums for exceptions to make returning exception messages consistent. Added enum for currencies to make using currencies strings consistent

---
+++ StocksPortfolio.Application.Interfaces/Enums/Currencies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksPortfolio.Application.Interfaces.Enums
+{
+    public static class Currencies
+    {
+        public const string Usd = "USD";
+    }
+}
---
+++ StocksPortfolio.Application.Interfaces/Enums/ExceptionCodes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StocksPortfolio.Application.Interfaces.Enums
+{
+    public static class ExceptionCodes
+    {
+        public const string portfolioDoesNotExist = "Portfolio does not exist";
+        public const string currencyDoesNotExist = "Currency does not exist";
+        public const string currencyNotSupported = "Currency not supported";
+        public const string notAValid24String = "Is not a valid 24 digit string";
+        public const string errorWhileDeletingPortfolio = "Error while deleting portfolio";
+        public const string errorWhileUpdatingCurrenciesExchange = "Error while updating currencies exchange";
+    }
+}
