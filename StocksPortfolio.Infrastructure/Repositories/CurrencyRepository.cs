06e984c78531b5471b48f436f838935bb97a1962
Oliwier Kania <o2k.oliwierkacperkania@gmail.com>
2024-03-11 09:06:52 +0100

    Added method to update currencies. This is part of previous commit

--- StocksPortfolio.Infrastructure/Repositories/CurrencyRepository.cs
+++ StocksPortfolio.Infrastructure/Repositories/CurrencyRepository.cs
@@ -41,5 +41,37 @@ namespace StocksPortfolio.Infrastructure.Services
             return _mapper.Map<CurrencyCollection, CurrencyViewModel>(currencyCollection);
         }
 
+        public void Update(CurrencyCollection collectionToUpdate)
+        {
+            var filter = Builders<CurrencyCollection>.Filter.Eq(doc => doc.Id, collectionToUpdate.Id);
+            var updateTimestamp = Builders<CurrencyCollection>.Update.Set(doc => doc.Timestamp, collectionToUpdate.Timestamp);
+            long numberOfChanges;
+            numberOfChanges = _currencyCollection.UpdateOne(filter, updateTimestamp).ModifiedCount;
+
+            foreach (var quoteToUpdate in collectionToUpdate.Quotes)
+            {
+                var updateQuote = Builders<CurrencyCollection>.Update.Set(doc => doc.Quotes[-1].Value, quoteToUpdate.Value);
+                var filterQuote = Builders<CurrencyCollection>.Filter.And(
+                    Builders<CurrencyCollection>.Filter.Eq(doc => doc.Id, collectionToUpdate.Id),
+                    Builders<CurrencyCollection>.Filter.ElemMatch(doc => doc.Quotes,
+                    Builders<Quote>.Filter.Eq(q => q.type, quoteToUpdate.type))
+                );
+
+                numberOfChanges += _currencyCollection.UpdateOne(filterQuote, updateQuote).ModifiedCount;
+            }
+
+            if (numberOfChanges < 1)
+                throw new Exception(ExceptionCodes.errorWhileUpdatingCurrenciesExchange);
+
+            Save();
+        }
+
+        private void Save()
+        {
+
+            string rootProjectDirectoryPath = Directory.GetParent(Environment.CurrentDirectory).FullName;
+            var absolutePathToFile = Path.Combine(rootProjectDirectoryPath, Path.Combine(_mongoDbSettings.Value.CurrencySaveString));
+            _runner.Export(_mongoDbSettings.Value.CurrencyName, _mongoDbSettings.Value.CurrencyCollectionName, absolutePathToFile);
+        }
     }
 }
