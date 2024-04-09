ffa9812dc574d2709c869aeacb469804ebfb6ae5
Oliwier Kania <o2k.oliwierkacperkania@gmail.com>
2024-04-09 08:51:21 +0200

    Got rid of hardcoded test data, which shuld be only in db files

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using Domain;
+using Domain.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
@@ -30,48 +32,18 @@ namespace Tests
         private readonly Mock<IStocksService> _stocksService = new Mock<IStocksService>();
 
         private readonly CurrencyRepository _currencyRepositoryObj;
-        private readonly Mock<ICurrencyRepository> _currencyRepository = new Mock<ICurrencyRepository>(); 
+        private readonly Mock<ICurrencyRepository> _currencyRepository = new Mock<ICurrencyRepository>();
 
         private readonly CurrencyService _currencyServiceObj;
         private readonly Mock<ICurrencyService> _currencyService = new Mock<ICurrencyService>();
 
-        public static readonly PortfolioViewModel portfolioSample = new PortfolioViewModel
-        {
-            Id = ObjectId.Parse("50227b375dff9218248eadc4"),
-            CurrentTotalValue = 0,
-            IsDeleted = false,
-            Stocks = new List<StockCollection>
-            {
-                new StockCollection { Ticker = "AAPL", Currency = "USD", NumberOfShares = 20 },
-                new StockCollection { Ticker = "TSLA", Currency = "USD", NumberOfShares = 100 },
-                new StockCollection { Ticker = "NVDA", Currency = "USD", NumberOfShares = 50 },
-                new StockCollection { Ticker = "ALLEGRO", Currency = "USD", NumberOfShares = 19000 },
-                new StockCollection { Ticker = "ADR", Currency = "USD", NumberOfShares = 1100 }
-            }
-        };
-
-        public static readonly CurrencyViewModel currencyViewModel = new CurrencyViewModel
-        {     
-            Id = ObjectId.Parse("00227b375dff9218248edfd4"),
-            Timestamp = 0,
-            Quotes = CurrencyExchangeContext.quotes
-        };
-
-        public static readonly List<PortfolioViewModel> portfolioSampleList = new List<PortfolioViewModel> { portfolioSample };
-        public static readonly StockModel stockModel = new StockModel 
-        {
-            Price = 1 ,
-            Currency = ""
-        };
-
         public Tests()
         {
-
             var builder = WebApplication.CreateBuilder();
             var configuration = builder.Configuration;
             builder.Services.AddHttpClient("currencyExchangeApi", client =>
             {
-                client.BaseAddress = new Uri($"{configuration["currencyExchangeUrl"]}live?access_key={configuration["currencyExchangeUrl"]}");
+                client.BaseAddress = new Uri(configuration["currencyExchangeUrl"]);
             });
 
             var serviceProvider = builder.Services.BuildServiceProvider();
@@ -84,35 +56,33 @@ namespace Tests
 
             _mapper = mapperConfig.CreateMapper();
 
-            MongoDbSettings mongoDbSettings = new MongoDbSettings
-            {
-                PortfolioName = "portfolioDb",
-                PortfolioConnectionString = new string[] { "Data","portfoliosMock.json"},
-                PortfolioCollectionName = "Portfolios",
-                CurrencyName = "currencyDb",
-                CurrencyConnectionString = new string[] { "Data", "currenciesMock.json"},
-                CurrencyCollectionName = "Currencies",
-                PortfolioSaveString = new string[] { "" },
-                CurrencySaveString = new string[] { "" }
-            };
+            builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDB"));
+
+            var app = builder.Build();
+            var serviceProvider2 = app.Services;
+            var mongoDbSettings = serviceProvider2.GetRequiredService<IOptions<MongoDbSettings>>().Value;
+
             IOptions<MongoDbSettings> optionsMongoDbSettings = Options.Create(mongoDbSettings);
 
             _portfolioRepositoryObj = new PortfolioRepository(optionsMongoDbSettings, _mapper);
+            _currencyRepositoryObj = new CurrencyRepository(optionsMongoDbSettings, _mapper);
             _portfolioServiceObj = new PortfolioService(_portfolioRepository.Object, _mapper, _stocksService.Object);
-            _currencyServiceObj = new CurrencyService(_currencyRepository.Object, _mapper, httpClientFactory);
+            _currencyServiceObj = new CurrencyService(_currencyRepository.Object, _mapper, httpClientFactory, configuration);
+            _stocksServiceObj = new StocksService();
         }
 
         [Theory]
-        [InlineData("50227b375dff9218248eadc4")]
-        public async void CheckIfPortfolioServiceGetPortfolioByIdReturnsCorrectObject(string id)
+        [InlineData("00227b375dff9218248eadc4")]
+        public async void CheckIfPortfolioServiceGetPortfolioByIdThatIsNotSoftDeleteReturnsCorrectObject(string portfolioId)
         {
             //Arrange
-            var objectId = ObjectId.Parse(id);
-            _portfolioService.Setup(x => x.GetPortfolio(id)).Returns(portfolioSample);
+            var portfolioSample = _portfolioRepositoryObj.GetPortfolioByIdThatIsNotSoftDeleted(ObjectId.Parse(portfolioId));
+            var objectId = ObjectId.Parse(portfolioId);
+            _portfolioService.Setup(x => x.GetPortfolio(portfolioId)).Returns(portfolioSample);
             _portfolioRepository.Setup(x => x.GetPortfolioByIdThatIsNotSoftDeleted(objectId)).Returns(portfolioSample);
 
             //Act
-            var result = _portfolioServiceObj.GetPortfolio(id);
+            var result = _portfolioServiceObj.GetPortfolio(portfolioId);
 
             //Assert
             Assert.Equal(result.Id, objectId);
@@ -121,14 +91,15 @@ namespace Tests
         [Theory]
         [InlineData("00227b375dff9218248eadc4", false, 5, "AAPL")]
         [InlineData("00227b375dff9218248eadc6", false, 5, "ADR")]
-        public async void CheckIfPortfolioRepositoryGetPortfolioByIdThatIsNotSoftDeletedReturnsCorrectObject(string id, bool isDeleted, int numberOfStocks, string firstTicker)
+        public async void CheckIfPortfolioRepositoryGetPortfolioByIdThatIsNotSoftDeletedReturnsCorrectObject(string portfolioId, bool isDeleted, int numberOfStocks, string firstTicker)
         {
             //Arrange
-            var objectId = ObjectId.Parse(id);
-            _portfolioRepository.Setup(x => x.GetPortfolioByIdThatIsNotSoftDeleted(ObjectId.Parse(id))).Returns(portfolioSample);
+            var objectId = ObjectId.Parse(portfolioId);
+            var portfolioSample = _portfolioRepositoryObj.GetPortfolioByIdThatIsNotSoftDeleted(ObjectId.Parse(portfolioId));
+            _portfolioRepository.Setup(x => x.GetPortfolioByIdThatIsNotSoftDeleted(ObjectId.Parse(portfolioId))).Returns(portfolioSample);
 
             //Act
-            var result = _portfolioRepositoryObj.GetPortfolioByIdThatIsNotSoftDeleted(ObjectId.Parse(id));
+            var result = _portfolioRepositoryObj.GetPortfolioByIdThatIsNotSoftDeleted(ObjectId.Parse(portfolioId));
 
             //Assert
             Assert.Equal(result.Id, objectId);
@@ -139,11 +110,12 @@ namespace Tests
 
         [Theory]
         [InlineData("00227b375dff9218248eadc4")]
-        public async void CheckIfPortfolioServiceGetPortfolioThrowsErrorWhenIdDoNotExist(string id)
+        public async void CheckIfPortfolioServiceGetPortfolioThrowsErrorWhenIdDoNotExist(string portfolioId)
         {
             //Arrange
-            var objectId = ObjectId.Parse(id);
-            _portfolioService.Setup(x => x.GetPortfolio(id)).Throws(new NullReferenceException(ExceptionCodes.portfolioDoesNotExist));
+            var objectId = ObjectId.Parse(portfolioId);
+            _portfolioService.Setup(x => x.GetPortfolio(portfolioId)).Throws(new NullReferenceException(ExceptionCodes.portfolioDoesNotExist));
+            var portfolioSample = _portfolioRepositoryObj.GetPortfolioByIdThatIsNotSoftDeleted(ObjectId.Parse(portfolioId));
             _portfolioRepository.Setup(x => x.GetPortfolioByIdThatIsNotSoftDeleted(objectId)).Returns(portfolioSample);
 
             //Act
@@ -158,6 +130,7 @@ namespace Tests
         public async void CheckIfMethodReturnsOnlyDocumnetsWithFieldIsDeletedAsFalse(int numberOfElements)
         {
             //Arrange
+            var portfolioSampleList = _portfolioRepositoryObj.GetAllPortfoliosThatAreNotSoftDeleted();
             _portfolioRepository.Setup(x => x.GetAllPortfoliosThatAreNotSoftDeleted()).Returns(portfolioSampleList);
             //Act
             var result = _portfolioRepositoryObj.GetAllPortfoliosThatAreNotSoftDeleted();
@@ -167,13 +140,16 @@ namespace Tests
 
 
         [Theory]
-        [InlineData("00227b375dff9218248eadc4", Currencies.Usd, 20270)]
-        public async void CheckIfTotalValueIsCorrectlyAdded(string portfolioId, string currency, int value)
+        [InlineData("00227b375dff9218248eadc4", Currencies.Usd, 20270, "")]
+        public async void CheckIfTotalValueIsCorrectlyAdded(string portfolioId, string currency, int value, string ticker)
         {
             //Arrange
             var objectId = ObjectId.Parse(portfolioId);
-            _stocksService.Setup(x => x.GetStockPrice(It.IsAny<string>())).ReturnsAsync(stockModel);
+            var stockPrice = await _stocksServiceObj.GetStockPrice(ticker);
+            _stocksService.Setup(x => x.GetStockPrice(It.IsAny<string>())).ReturnsAsync(stockPrice);
+            var currencyViewModel = _currencyRepositoryObj.GetAll();
             _currencyRepository.Setup(x => x.GetAll()).Returns(currencyViewModel);
+            var portfolioSample = _portfolioRepositoryObj.GetPortfolioByIdThatIsNotSoftDeleted(ObjectId.Parse(portfolioId));
             _portfolioRepository.Setup(x => x.GetPortfolioByIdThatIsNotSoftDeleted(objectId)).Returns(portfolioSample);
             _portfolioService.Setup(x => x.GetPortfolio(portfolioId)).Returns(portfolioSample);
             _currencyService.Setup(x => x.GetCurrencyExchangeData()).Returns(currencyViewModel);
@@ -181,13 +157,13 @@ namespace Tests
             _portfolioService.Setup(x => x.GetTotalPortfolioValue(portfolioViewModelMock, currency, currencyViewModel)).ReturnsAsync(new decimal(1));
 
             //Act
-            var portfolio =  _portfolioServiceObj.GetPortfolio(portfolioId);
+            var portfolio = _portfolioServiceObj.GetPortfolio(portfolioId);
             var currencyData = _currencyServiceObj.GetCurrencyExchangeData();
             var portfolioViewModel = _mapper.Map<PortfolioViewModel, PortfolioCollection>(portfolio);
-            var totalAmount =  await _portfolioServiceObj.GetTotalPortfolioValue(portfolioViewModel, currency, currencyData);
+            var totalAmount = await _portfolioServiceObj.GetTotalPortfolioValue(portfolioViewModel, currency, currencyData);
 
             //Assert
-            Assert.Equal(totalAmount, value);
+            Assert.Equal(totalAmount, value * stockPrice.Price);
         }
     }
 }
